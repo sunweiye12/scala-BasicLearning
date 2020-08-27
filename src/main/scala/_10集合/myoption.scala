ea83cce9c8866714794f0c539c83172877c5e77b
sunweiye.3 <sunweiye.3@bytedance.com>
2020-08-27 16:58:04 +0800

    2

--- src/main/scala/_10集合/myoption.scala
+++ src/main/scala/_10集合/myoption.scala
@@ -21,7 +21,7 @@ object myoption {
     //  Option[String] 通过此类型的应用告诉我们 程序会尽量给我们传回一个String对象，如果没有则返回None
     //  Option对象也只是一个类,Some和None是他的一个子类,入过能找到返回值就会用Some包装返回,否则直接返回一个None对象
     println(value1.)      // Some(value) // 直接返回的是一个Some对象
-    println(value1.get)   // 当返回的是Some对象的是后说明返回成功了,就可以通过get方法得到里面的内容
+    println(value1.get)   // 当返回的是Some对象的是后说明返回成功了 就可以通过get方法得到里面的内容
     println(value2)
     println(value3.getOrElse(None)) // None   // 如果不确定返回的是Some还是None就直接执行getOrElse
   }
